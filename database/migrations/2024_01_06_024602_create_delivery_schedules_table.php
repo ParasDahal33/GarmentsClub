c411f476ea527bb5bfc323494b3e28bd50852329
ParasDahal33 <paras.dahal2016@gmail.com>
2024-01-06 08:32:08 +0545

    Add create_delivery_schedules_table migration

---
+++ database/migrations/2024_01_06_024602_create_delivery_schedules_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('delivery_schedules', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('order_id')->constrained();
+            $table->dateTime('pickup_time');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('delivery_schedules');
+    }
+};
